544301adbcbb839e8ab954ccb7979c0257c10bad
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-07-02 02:38:05 +0300

    📚 chore: establishing a connection to database

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-import express from 'express'
+import express from 'express';
+import dotenv from 'dotenv';
+import connectToMongoDB from './db/connection.js';
+
+dotenv.config();
 const app = express()
-const port = 5000
+const PORT = process.env.PORT || 8000;
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+
+connectToMongoDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT} 📟`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to MongoDB, server not started 🙍🏻‍♂️", error);
+  });

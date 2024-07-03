2a57058a3213b2b05e202745d8abaea4e29e41b8
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-07-03 06:33:13 +0300

    📚 feat: creating error handling middleware

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectToMongoDB from './db/connection.js';
+import { globalErrorHandler, notFound } from './middlewares/error.middleware.js';
+import authorRoutes from './routes/author.routes.js';
+import bookRoutes from './routes/book.routes.js';
 
 dotenv.config();
 const app = express()
 const PORT = process.env.PORT || 8000;
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/authors", authorRoutes);
+app.use("/api/books", bookRoutes);
+app.use(notFound);
+app.use(globalErrorHandler);
 
 connectToMongoDB()
   .then(() => {

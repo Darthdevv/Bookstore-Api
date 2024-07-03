815b47aab861d4af2d9e71b35a2ea0cf1f911b6a
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-07-03 06:35:44 +0300

    📚 feat: adding cors to project

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import connectToMongoDB from './db/connection.js';
 import { globalErrorHandler, notFound } from './middlewares/error.middleware.js';
 import authorRoutes from './routes/author.routes.js';
@@ -8,6 +9,7 @@ import bookRoutes from './routes/book.routes.js';
 dotenv.config();
 const app = express()
 const PORT = process.env.PORT || 8000;
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/authors", authorRoutes);

32b3ae13cc8bbfaf8218e2400f430ec10068ddf9
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-07-03 05:34:22 +0300

    📚 feat: author and book models are added

--- models/author.model.js
+++ models/author.model.js
@@ -1,6 +1,24 @@
 import mongoose from "mongoose";
 
-const authorSchema = new mongoose.Schema({});
+const authorSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    bio: {
+      type: String
+    },
+    birthDate: {
+      type: Date
+    },
+    books: [{
+      type: Schema.Types.ObjectId,
+      ref: "Book"
+    }],
+  },
+  {timestamps: true}
+);
 
 const Author = mongoose.model("Author", bookSchema);
 
--- models/book.model.js
+++ models/book.model.js
@@ -1,9 +1,27 @@
 import mongoose from "mongoose";
 
 
-const bookSchema = new mongoose.Schema({
-
-});
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    publishedDate: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
 
 
 const Book = mongoose.model("Book", bookSchema);
